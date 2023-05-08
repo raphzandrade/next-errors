46f7554e6315adee15cebe1bef2cbbeefb85171b
raphzandrade <raphz.andrade@gmail.com>
2023-05-08 10:40:10 -0300

    testing new version

--- apps/my-app/jest.config.ts
+++ apps/my-app/jest.config.ts
@@ -1,19 +1,19 @@
-import nextJest from 'next/jest'
+import nextJest from 'next/jest';
 
 const createJestConfig = nextJest({
   dir: 'apps/my-app',
-})
+});
 
 const baseJestConfig = {
-  displayName: "my-app",
-  preset: "../../jest.preset.js",
+  displayName: 'my-app',
+  preset: '../../jest.preset.js',
   transform: {
-    "^(?!.*\\.(js|jsx|ts|tsx|css|json)$)": "@nrwl/react/plugins/jest",
-    "^.+\\.[tj]sx?$": ["babel-jest", { presets: ["@nrwl/next/babel"] }],
+    '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nx/react/plugins/jest',
+    '^.+\\.[tj]sx?$': ['babel-jest', { presets: ['@nx/next/babel'] }],
   },
-  moduleFileExtensions: ["ts", "tsx", "js", "jsx"],
-  coverageDirectory: "../../coverage/apps/my-app",
-}
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  coverageDirectory: '../../coverage/apps/my-app',
+};
 
 // export default baseJestConfig;
-export default createJestConfig(baseJestConfig)
+export default createJestConfig(baseJestConfig);
--- apps/my-app/next.config.js
+++ apps/my-app/next.config.js
@@ -1,15 +1,21 @@
 //@ts-check
 
 // eslint-disable-next-line @typescript-eslint/no-var-requires
-const { withNx } = require("@nrwl/next/plugins/with-nx");
+const { composePlugins } = require('@nx/next');
+const { withNx } = require('@nx/next/plugins/with-nx');
 
 const removeImports = require('next-remove-imports')({
   test: /node_modules([\s\S]*?)\.(tsx|ts|js|mjs|jsx)$/,
-  matchImports: "\\.(less|css|scss|sass|styl)$"
-})
+  matchImports: '\\.(less|css|scss|sass|styl)$',
+});
+
+const plugins = [
+  removeImports,
+  withNx
+]
 
 /**
- * @type {import('@nrwl/next/plugins/with-nx').WithNxOptions}
+ * @type {import('@nx/next/plugins/with-nx').WithNxOptions}
  **/
 const nextConfig = {
   nx: {
@@ -19,6 +25,8 @@ const nextConfig = {
   },
 };
 
-
 // module.exports = removeImports(withNx(nextConfig));
-module.exports = withNx(nextConfig);
+// module.exports = withNx(nextConfig);
+module.exports = composePlugins(...plugins)({
+  nextConfig
+})
